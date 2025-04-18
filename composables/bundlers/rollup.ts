285f1b3922349a4e6b1986ff3e2e308172b2f9b4
Alexander Lichter <github@lichter.io>
2025-04-18 21:53:21 +0200

    feat: basic ts implementation

--- composables/bundlers/rollup.ts
+++ composables/bundlers/rollup.ts
@@ -1,5 +1,6 @@
 import { rollup as build, type LogLevel, type RollupLog } from '@rollup/browser'
 import type { Bundler } from './index'
+import ts from 'typescript';
 
 export const rollup: Bundler = {
   id: 'rollup',
@@ -7,7 +8,7 @@ export const rollup: Bundler = {
   icon: 'i-logos:rollupjs',
   pkgName: '@rollup/browser',
   async build(code, config) {
-    const entry = '_virtual-entry.js'
+    const entry = '_virtual-entry.ts'
     const warnings: string[] = []
     const bundle = await build({
       input: [entry],
@@ -28,6 +29,23 @@ export const rollup: Bundler = {
           load(id) {
             return id === entry ? code : null
           },
+        },
+        {
+          name: 'typescript',
+          filter: /\.ts$/,
+          transform(code) {
+            const result = ts.transpileModule(code, {
+              compilerOptions: {
+                module: ts.ModuleKind.ESNext,
+                target: ts.ScriptTarget.ESNext,
+                sourceMap: true,
+              },
+            });
+            return {
+              code: result.outputText,
+              map: result.sourceMapText ? JSON.parse(result.sourceMapText) : null,
+            };
+          }
         },
         config?.plugins,
       ],
